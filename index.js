48430e6922f2004b554c96d673dd0435343732fa
Bob W. Hogg <rwhogg@linux.com>
2017-02-25 11:05:37 -0800

    Add initial rules
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ module.exports =
 {
     env:
     {
-        "browser": true,
-        "es6": true,
-        "node": true
+        amd: true,
+        browser: true,
+        es6: true,
+        node: true
+    },
+    parserOptions:
+    {
+        ecmaFeatures:
+        {
+            globalReturn: true
+        },   
+        ecmaVersion: 2017
     },
     extends: "eslint:recommended",
     rules:
@@ -28,6 +37,61 @@ module.exports =
         [
             "error",
             "always"
+        ],
+        "block-scoped-var": "error",
+        "consistent-return": "error",
+        curly: "error",
+        "dot-location":
+        [
+            "error",
+            "property"
+        ],
+        "dot-notation": "error",
+        eqeqeq:
+        [
+            "error",
+            "smart"
+        ],
+        "no-else-return": "error",
+        "no-eval": "error",
+        "no-fallthrough": "off",
+        "no-iterator": "error",
+        "no-lone-blocks": "error",
+        "no-magic-numbers":
+        [
+            "error",
+            {
+                ignoreArrayIndexes: true,
+                ignore: [0, 1, 2, 4, 5, 10]
+            }
+        ],
+        "no-multi-spaces": "error",
+        "no-new-wrappers": "error",
+        "no-proto": "error",
+        "no-return-assign":
+        [
+            "error",
+            "always"
+        ],
+        "no-self-compare": "error",
+        "no-sequences": "error",
+        "no-useless-escape": "error",
+        "no-useless-return": "error",
+        "no-with": "error",
+        yoda: "error",
+        "no-undefined": "error",
+        // FIXME: have not added many options from http://eslint.org/docs/rules/#stylistic-issues
+        "brace-style":
+        [
+            "error",
+            "allman"
+        ],
+        "comma-dangle":
+        [
+            "error",
+            // this is technically the default
+            // however, it's better to be explicit
+            "never"
         ]
     }
 };
