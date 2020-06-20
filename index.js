aec4bd993cc88b8338d15104db5679bbc067ada8
Bob Wombat Hogg <wombat@rwhogg.site>
2020-06-20 19:58:09 -0400

    Partially reorganize

--- index.js
+++ index.js
@@ -13,8 +13,65 @@ module.exports = {
         ecmaVersion: 10,
     },
     plugins: ["fp"],
+    // FIXME: you should stop doing this and inline all the rules so you always know what you're doing
     extends: "eslint:recommended",
     rules: {
+        "array-bracket-newline": [
+            err,
+            {multiline: true},
+        ],
+        "array-element-newline": [
+            err,
+            {ArrayExpression: "consistent"},
+        ],
+        "array-bracket-spacing": [
+            err,
+            "never",
+        ],
+        "arrow-body-style": [
+            err,
+            "as-needed",
+        ],
+        camelcase: [
+            err,
+            {properties: "never"},
+        ],
+        "block-scoped-var": err,
+        "block-spacing":[
+            err,
+            "always",
+        ],
+        "brace-style": [
+            err,
+            "stroustrup",
+        ],
+        "comma-dangle": [
+            err,
+            "always-multiline",
+        ],
+        "comma-spacing": err,
+        "comma-style": err,
+        "computed-property-spacing": err,
+        "consistent-return": err,
+        curly: err,
+        "dot-location": [
+            err,
+            "property",
+        ],
+        "dot-notation": err,
+        "eol-last": [
+            err,
+            "always",
+        ],
+        eqeqeq: [
+            err,
+            "smart",
+        ],
+        "for-direction": err,
+        "func-call-spacing": [
+            err,
+            "never",
+        ],
         indent: [
             err,
             4,
@@ -26,34 +83,7 @@ module.exports = {
             err,
             "unix",
         ],
-        quotes: [
-            err,
-            "double",
-        ],
-        semi: [
-            err,
-            "never",
-        ],
-        "arrow-body-style": [
-            err,
-            "as-needed",
-        ],
-        "block-scoped-var": err,
-        "block-spacing":[
-            err,
-            "always",
-        ],
-        "consistent-return": err,
-        curly: err,
-        "dot-location": [
-            err,
-            "property",
-        ],
-        "dot-notation": err,
-        eqeqeq: [
-            err,
-            "smart",
-        ],
+        "max-len": [err, {"code": 120}],
         "new-cap": err,
         "new-parens": err,
         "no-console": "off",
@@ -80,59 +110,15 @@ module.exports = {
         "no-sequences": err,
         "no-useless-escape": err,
         "no-useless-return": err,
-        "no-with": err,
-        yoda: err,
         "no-undefined": err,
         "no-underscore-dangle": [
             err,
             {enforceInMethodNames: true},
         ],
-        "comma-dangle": [
-            err,
-            "always-multiline",
-        ],
-        "comma-spacing": err,
-        "comma-style": err,
-        "array-bracket-newline": [
-            err,
-            {multiline: true},
-        ],
-        "array-element-newline": [
-            err,
-            {ArrayExpression: "consistent"},
-        ],
-        "array-bracket-spacing": [
-            err,
-            "never",
-        ],
-        camelcase: [
-            err,
-            {properties: "never"},
-        ],
-        "fp/no-loops": err,
-        "fp/no-arguments": err,
-        "valid-jsdoc": err,
-        "eol-last": [
-            err,
-            "always",
-        ],
-        "func-call-spacing": [
-            err,
-            "never",
-        ],
-        "brace-style": [
-            err,
-            "stroustrup",
-        ],
-        "computed-property-spacing": err,
         "no-lonely-if": err,
         "no-trailing-spaces": err,
-        "unicode-bom": [
-            err,
-            "never",
-        ],
         "no-var": err,
-        "for-direction": err,
+        "no-with": err,
         "no-compare-neg-zero": err,
         "no-constant-condition": err,
         "no-dupe-args": err,
@@ -142,17 +128,32 @@ module.exports = {
         "no-unexpected-multiline" :err,
         "no-unreachable": err,
         "no-unsafe-finally": err,
-        "require-atomic-updates": err,
-        "valid-typeof": err,
         "no-extra-bind": err,
         "no-proto": err,
         "no-return-await": err,
         "no-useless-call": err,
-        "jsx-quotes": [err, "prefer-double"], // FIXME: should be moved to React-specific ruleset
-        "max-len": [err, {"code": 120}],
         "no-tabs": err,
         "no-this-before-super": err,
-        "prefer-exponentiation-operator": err,
         "padded-blocks": [err, "never"],
+        "prefer-exponentiation-operator": err,
+        quotes: [
+            err,
+            "double",
+        ],
+        "require-atomic-updates": err,
+        semi: [
+            err,
+            "never",
+        ],
+        "unicode-bom": [
+            err,
+            "never",
+        ],
+        "valid-jsdoc": err,
+        "valid-typeof": err,
+        yoda: err,
+        "fp/no-loops": err,
+        "fp/no-arguments": err,
+        "jsx-quotes": [err, "prefer-double"], // FIXME: should be moved to React-specific ruleset
     },
 }
